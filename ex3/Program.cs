3e3dc1af9b368f4f971e9d03f6e49aa72b23588d
ElenaShamiryan <elen91@list.ru>
2022-07-20 12:31:39 +0300

    таблица кубов

---
+++ ex3/Program.cs
@@ -0,0 +1,10 @@
+﻿void Cube (int a)
+{
+for (int b=1; b<=a; b++)
+{
+Console.Write(b*b*b+" ");
+}
+}
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Cube (num);
