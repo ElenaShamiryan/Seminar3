2044b411cbaef9037399a1c12d4bf6cb59fff32b
ElenaShamiryan <elen91@list.ru>
2022-07-20 12:25:36 +0300

    палиндром

---
+++ ex1/Program.cs
@@ -0,0 +1,26 @@
+﻿void Palindrom(int n)
+{
+    if (n > 99999) Console.WriteLine("Введите пятизначное число");
+    if (n < 99999)
+    {
+        int index = 0;
+        string s1 = Convert.ToString(n);
+        int length = s1.Length;
+        for (; s1[index] == s1[length - 1];)
+        {
+            index++;
+            length--;
+            Console.Write("число является палиндромом");
+            break;
+        }
+        for (; s1[index] != s1[length - 1];)
+        {
+            Console.WriteLine("число НЕ является палиндромом");
+            break;
+        }
+    }
+}
+
+Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+Palindrom(num);
